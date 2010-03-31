c0777b1a96293508598e4a58ac8f355571af87b8
John Evans <john@milo.com>
2010-03-31 14:51:32 -0700

    Initial commit.

---
+++ wasitacrawler.py
@@ -0,0 +1,26 @@
+import os
+
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext.webapp import template
+
+
+class MainPage(webapp.RequestHandler):
+
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/html'
+        self.response.out.write(
+            template.render("main.html", {}))
+
+
+application = webapp.WSGIApplication(
+                                     [('/', MainPage)],
+                                     debug=True)
+
+
+def main():
+    run_wsgi_app(application)
+
+
+if __name__ == "__main__":
+    main()
